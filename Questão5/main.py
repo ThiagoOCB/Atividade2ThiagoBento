b67b82fe4ba7f8b35de48c8b608df434487777c1
Thiago <64999819+ThiagoOCB@users.noreply.github.com>
2020-11-18 15:23:50 -0300

    Delete main.py

--- Questão5/main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: latin1 -*-
-
-def inversao_palavras(frase_normal):
-   frase = str(frase_normal)
-   invertida = ' ' .join(palavra[::-1] for palavra in frase.split())
-   return invertida
-
-print('frase invertida: ', inversao_palavras('O céu é azul'))
