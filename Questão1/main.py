c3d6d3f806a2cca47aff0509f0649a64a60777f7
Thiago <64999819+ThiagoOCB@users.noreply.github.com>
2020-11-18 15:12:21 -0300

    Delete main.py

--- Questão1/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: latin 1 -*-
-id = int(input('Digite sua idade em dias:'))
-
-ia = int (id / 365) #onde ia é a idade em anos
-im =int((id % 365) / 30) #onde im é a idade em meses
-rd = (id % 365) % 30 #onde rd é dias restantes
-
-print ('A idade em anos, meses e dias =', ia, 'anos', im, 'meses', rd, 'dias')
